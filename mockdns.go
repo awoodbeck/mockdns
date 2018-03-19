713effdc282d681fe3592a4a3599a02cfbdafccd
Adam Woodbeck <adam@woodbeck.net>
2018-03-19 14:27:36 -0400

    Allow proxying of requests through mock DNS

--- mockdns.go
+++ mockdns.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -27,8 +28,12 @@ const (
 var (
 	addr,
 	dataFile,
-	defaultTTL string
-	verbose            bool
+	defaultTTL,
+	resolvConfFile string
+	proxy,
+	verbose bool
+	client             *dns.Client
+	clientConfig       *dns.ClientConfig
 	errNilMapUnmarshal = errors.New("cannot unmarshal into nil map")
 
 	supportedTypes = map[string]uint16{
@@ -42,14 +47,19 @@ var (
 		"TXT":   dns.TypeTXT,
 	}
 
-	cFailure = color.New(color.FgRed)
-	cSuccess = color.New(color.FgGreen)
+	cFailure  = color.New(color.FgRed).Sprint("F")
+	cSuccess  = color.New(color.FgGreen).Sprint("S")
+	cOverride = color.New(color.FgYellow).Sprint("O")
+	cProxied  = color.New(color.FgBlue).Sprint("P")
+	cTerminal = color.New(color.FgRed).Sprint("T")
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", "127.0.0.1:8053", "default listening address")
 	flag.StringVar(&dataFile, "data", "", "DNS record data file")
 	flag.StringVar(&defaultTTL, "ttl", "3600", "default TTL")
+	flag.StringVar(&resolvConfFile, "resolv", "/etc/resolv.conf", "resolv.conf file path")
+	flag.BoolVar(&proxy, "proxy", true, "proxy unmatched requests to root name servers")
 	flag.BoolVar(&verbose, "v", true, "verbose output")
 }
 
@@ -58,6 +68,18 @@ func main() {
 
 	if dataFile == "" {
 		log.Fatal("Data file required")
+	}
+
+	var err error
+	if proxy {
+		clientConfig, err = dns.ClientConfigFromFile(resolvConfFile)
+		if err != nil {
+			log.Fatalf("Reading %q: %s", resolvConfFile, err)
+		}
+		if len(clientConfig.Servers) == 0 {
+			log.Fatalf("No name servers found in %q", resolvConfFile)
+		}
+		client = new(dns.Client)
 	}
 
 	b, err := ioutil.ReadFile(dataFile)
@@ -93,10 +115,10 @@ func main() {
 
 func serve(ctx context.Context, addr, net string, d data) {
 	for domain, recs := range d {
-		dns.HandleFunc(domain, handler(recs))
+		dns.HandleFunc(domain, logRequest(true, handler(recs)))
 	}
 
-	dns.HandleFunc(".", catchAllHandler)
+	dns.HandleFunc(".", logRequest(false, proxyHandler))
 
 	server := &dns.Server{Addr: addr, Net: net, TsigSecret: nil}
 
@@ -116,26 +138,8 @@ func serve(ctx context.Context, addr, net string, d data) {
 	log.Printf("%s/%s listener stopped\n", addr, net)
 }
 
-func catchAllHandler(w dns.ResponseWriter, r *dns.Msg) {
-	if verbose {
-		for _, q := range r.Question {
-			log.Printf("[%s]: %s", cFailure.Sprint("F"), q.String())
-		}
-	}
-
-	m := new(dns.Msg)
-	m.SetRcode(r, dns.RcodeServerFailure)
-	w.WriteMsg(m)
-}
-
 func handler(recs records) func(dns.ResponseWriter, *dns.Msg) {
 	return func(w dns.ResponseWriter, r *dns.Msg) {
-		if verbose {
-			for _, q := range r.Question {
-				log.Printf("[%s]: %s", cSuccess.Sprint("S"), q.String())
-			}
-		}
-
 		m := new(dns.Msg)
 		m.SetReply(r)
 
@@ -166,5 +170,58 @@ func handler(recs records) func(dns.ResponseWriter, *dns.Msg) {
 		}
 
 		w.WriteMsg(m)
+	}
+}
+
+func proxyHandler(w dns.ResponseWriter, r *dns.Msg) {
+	var m *dns.Msg
+	err := errors.New("not proxied")
+
+	if proxy {
+		for _, ns := range clientConfig.Servers {
+			m, _, err = client.Exchange(r, fmt.Sprintf("%s:%s", ns, clientConfig.Port))
+			if err == nil {
+				break
+			}
+		}
+	}
+
+	if err != nil {
+		if m == nil {
+			m = new(dns.Msg)
+		}
+		m.SetRcode(r, dns.RcodeServerFailure)
+		r.Rcode = dns.RcodeServerFailure
+	}
+
+	w.WriteMsg(m)
+}
+
+func logRequest(local bool, f func(dns.ResponseWriter, *dns.Msg)) func(dns.ResponseWriter, *dns.Msg) {
+	return func(w dns.ResponseWriter, r *dns.Msg) {
+		f(w, r)
+		if verbose {
+			var t, res string
+			switch {
+			case local:
+				t = cOverride
+			case proxy:
+				t = cProxied
+			default:
+				t = cTerminal
+			}
+
+			// We don't have access to the reply Rcode, so we'll rely on the fact that
+			// we mirror the reply Rcode to the request for its reference in middleware.
+			if r.Rcode == 0 {
+				res = cSuccess
+			} else {
+				res = cFailure
+			}
+
+			for _, q := range r.Question {
+				log.Printf("[%s,%s]: %s", t, res, strings.TrimLeft(q.String(), ";"))
+			}
+		}
 	}
 }
